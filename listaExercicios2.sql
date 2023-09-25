b6a08c5cec441cb1b754276b4176bd02d2337ace
Joao Pedro <jp29.05.2006@gmail.com>
2023-09-25 20:58:04 -0300

    procedure 3 feito

--- listaExercicios2.sql
+++ listaExercicios2.sql
@@ -23,3 +23,19 @@ DELIMITER ;
 call sp_LivrosPorCategoria('Romance');
 call sp_LivrosPorCategoria('Ciência');
 drop procedure sp_LivrosPorCategoria;
+
+/*3*/
+delimiter //
+create procedure sp_ContarLivrosPorCategoria(in categoria_nome varchar(255), out total_livros int)
+begin
+    select COUNT(*) into total_livros
+    from Livro
+    inner join Categoria on Livro.Categoria_ID = Categoria.Categoria_ID
+    where Categoria.Nome = categoria_nome;
+end;
+//
+delimiter ;
+
+CALL sp_ContarLivrosPorCategoria('História', @total);
+select @total;
+drop procedure sp_ContarLivrosPorCategoria;
