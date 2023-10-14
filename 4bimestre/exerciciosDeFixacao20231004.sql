6e326ea4fb20f52b043465f843527ea487738194
JpOff01 <98951410+JpOff01@users.noreply.github.com>
2023-10-14 20:20:35 -0300

    exercicio 2

--- 4bimestre/exerciciosDeFixacao20231004.sql
+++ 4bimestre/exerciciosDeFixacao20231004.sql
@@ -30,3 +30,48 @@ BEGIN
 END //
 
 DELIMITER ;
+
+/*02*/
+DELIMITER //
+
+CREATE FUNCTION listar_livros_por_autor(
+    primeiro_nome_autor VARCHAR(255),
+    ultimo_nome_autor VARCHAR(255)
+) RETURNS TEXT
+BEGIN
+    DECLARE lista_livros TEXT;
+    DECLARE autor_id INT;
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE livro_id INT;
+    DECLARE livro_titulo VARCHAR(255);
+
+    SELECT id INTO autor_id FROM Autor WHERE primeiro_nome = primeiro_nome_autor AND ultimo_nome = ultimo_nome_autor;
+
+    SET lista_livros = '';
+
+    DECLARE cur CURSOR FOR 
+        SELECT LA.id_livro 
+        FROM Livro_Autor LA 
+        WHERE LA.id_autor = autor_id;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    OPEN cur;
+
+    read_loop: LOOP
+        FETCH cur INTO livro_id;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        SELECT titulo INTO livro_titulo FROM Livro WHERE id = livro_id;
+        SET lista_livros = CONCAT(lista_livros, livro_titulo, '; ');
+
+    END LOOP;
+
+    CLOSE cur;
+
+    RETURN lista_livros;
+END //
+
+DELIMITER ;
