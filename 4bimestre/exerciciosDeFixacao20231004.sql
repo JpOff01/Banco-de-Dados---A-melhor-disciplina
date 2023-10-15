4d7aad365a5eb9b8b26ac838cb94af47ae1cf263
JpOff01 <98951410+JpOff01@users.noreply.github.com>
2023-10-14 21:01:16 -0300

    atualizando exercicio 2

--- 4bimestre/exerciciosDeFixacao20231004.sql
+++ 4bimestre/exerciciosDeFixacao20231004.sql
@@ -22,49 +22,34 @@ DELIMITER ;
 SELECT total_livros_por_genero('Romance');
 
 /*02*/
-DELIMITER //
-
+delimiter //
 CREATE FUNCTION listar_livros_por_autor(
     primeiro_nome_autor VARCHAR(255),
     ultimo_nome_autor VARCHAR(255)
-) RETURNS TEXT
+) 
+RETURNS TEXT
+DETERMINISTIC
 BEGIN
     DECLARE lista_livros TEXT;
-    DECLARE autor_id INT;
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE livro_id INT;
-    DECLARE livro_titulo VARCHAR(255);
-
-    SELECT id INTO autor_id FROM Autor WHERE primeiro_nome = primeiro_nome_autor AND ultimo_nome = ultimo_nome_autor;
-
+    
     SET lista_livros = '';
 
-    DECLARE cur CURSOR FOR 
-        SELECT LA.id_livro 
-        FROM Livro_Autor LA 
-        WHERE LA.id_autor = autor_id;
+    SELECT GROUP_CONCAT(l.titulo SEPARATOR ', ') INTO lista_livros
+    FROM Livro l
+    JOIN Livro_Autor la ON l.id = la.id_livro
+    JOIN Autor a ON la.id_autor = a.id
+    WHERE a.primeiro_nome = primeiro_nome_autor AND a.ultimo_nome = ultimo_nome_autor;
+    
+    IF lista_livros IS NOT NULL THEN
+        RETURN lista_livros;
+    ELSE
+        RETURN 'Autor não encontrado.';
+    END IF;
+END;
+//
+delimiter ;
 
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
-    OPEN cur;
-
-    read_loop: LOOP
-        FETCH cur INTO livro_id;
-        IF done THEN
-            LEAVE read_loop;
-        END IF;
-
-        SELECT titulo INTO livro_titulo FROM Livro WHERE id = livro_id;
-        SET lista_livros = CONCAT(lista_livros, livro_titulo, '; ');
-
-    END LOOP;
-
-    CLOSE cur;
-
-    RETURN lista_livros;
-END //
-
-DELIMITER ;
+select listar_livros_por_autor('Carlos', 'Menezes');
 
 /*03*/
 DELIMITER //
