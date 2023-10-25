36efecd2bc1b891de42675530615a56d742b54cf
JpOff01 <98951410+JpOff01@users.noreply.github.com>
2023-10-25 20:12:14 -0300

    exercicio 2 feito

--- 4bimestre/exerciciosDeFixacao20231025.sql
+++ 4bimestre/exerciciosDeFixacao20231025.sql
@@ -1,3 +1,7 @@
 exercicio 1
 create trigger cliente_auditoria after insert on Clientes for each row
 insert into Auditoria (mensagem, data_hora) values ('Inserção em', now());
+
+exercicio 2
+create trigger remover_cliente before insert on Clientes for each row
+insert into Auditoria (mensagem, data_hora) values ('Tentativa de remover nome do indivíduo...', now());
